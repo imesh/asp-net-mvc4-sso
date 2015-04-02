bd129aaf50acf9a48332fd9e6f236250e0012e55
Imesh Gunaratne <imesh@apache.org>
2015-04-02 14:12:15 +0530

    Adding a sample web api

---
+++ TicketsApi/Controllers/TicketsController.cs
@@ -0,0 +1,48 @@
+﻿using TicketsApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace TicketsApi.Controllers
+{
+    public class TicketsController : ApiController
+    {
+        private static Ticket[] tickets = new Ticket[] 
+        {
+            new Ticket("1", "Bryan Adams Live", "NY"),
+            new Ticket("2", "Enrique Live", "Boston"),
+            new Ticket("3", "FIFA World Cup 2015", "AZ")
+        };
+      
+
+        // GET api/tickets
+        public IEnumerable<Ticket> GetAllTickets()
+        {
+            return tickets;
+        }
+
+        // GET api/tickets/5
+        public Ticket GetTicket(String id)
+        {
+            return tickets.FirstOrDefault((t) => t.ID.Equals(id));
+        }
+
+        // POST api/tickets
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/tickets/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/tickets/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ TicketsApi/Models/Ticket.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TicketsApi.Models
+{
+    public class Ticket
+    {
+        public String ID { get; set; }
+        public String Title { get; set; }
+        public String Venue { get; set; }
+
+        public Ticket()
+        {
+        }
+
+        public Ticket(String id, String title, String venue)
+        {
+            ID = id;
+            Title = title;
+            Venue = venue;
+        }
+    }
+}
