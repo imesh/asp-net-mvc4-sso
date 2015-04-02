e760aaddf572f4ab025f4ff2ebc89893585d10ec
Imesh Gunaratne <imesh@apache.org>
2015-04-02 14:44:31 +0530

    Updating SAML2 web application to invoke tickets api

---
+++ Mvc4SingleSignOnSAML2/Controllers/TicketsApiClient.cs
@@ -0,0 +1,25 @@
+﻿using Mvc4SingleSignOnSAML2.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Mvc4SingleSignOnSAML2.Controllers
+{
+    public class TicketsApiClient
+    {
+        readonly string uri = "http://localhost:2423/api/tickets";
+
+        public List<Ticket> GetTicketsAsync()
+        {
+            using (HttpClient httpClient = new HttpClient())
+            {
+                Task<String> response = httpClient.GetStringAsync(uri);
+                return JsonConvert.DeserializeObject<List<Ticket>>(response.Result);
+            }
+        }
+    }
+}
---
+++ Mvc4SingleSignOnSAML2/Controllers/TicketsController.cs
@@ -0,0 +1,24 @@
+﻿using Mvc4SingleSignOnSAML2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mvc4SingleSignOnSAML2.Controllers
+{
+    public class TicketsController : Controller
+    {
+        private TicketsApiClient apiClient = new TicketsApiClient();
+        //
+        // GET: /Tickets/
+
+        [Authorize]
+        public ActionResult Index()
+        {
+            List<Ticket> tickets = apiClient.GetTicketsAsync();
+            return View(tickets);
+        }
+
+    }
+}
